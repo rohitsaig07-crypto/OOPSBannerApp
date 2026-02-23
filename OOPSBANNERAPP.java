4d6271c46684cba04bea60accce57c6a509802a6
rohitsaig07-crypto <rohitsaig07@gmail.com>
2026-02-23 11:56:58 +0530

    uc4:using array and loop

--- OOPSBANNERAPP.java
+++ OOPSBANNERAPP.java
@@ -1,7 +1,33 @@
 public class OOPSBANNERAPP {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
-    }
 
+        String[] lines = new String[7];
+
+        lines[0] = String.join("",
+                "   ***   ", "   ***   ", "  ******  ", "   ***** ");
+
+        lines[1] = String.join("",
+                "  ** **  ", "  ** **  ", "  **   ** ", "  **   **");
+
+        lines[2] = String.join("",
+                " **   ** ", " **   ** ", "  **   ** ", "  **     ");
+
+        lines[3] = String.join("",
+                " **   ** ", " **   ** ", "  ******  ", "   ***** ");
+
+        lines[4] = String.join("",
+                " **   ** ", " **   ** ", "  **      ", "       **");
+
+        lines[5] = String.join("",
+                "  ** **  ", "  ** **  ", "  **      ", "  **   **");
+
+        lines[6] = String.join("",
+                "   ***   ", "   ***   ", "  **      ", "   ***** ");
+
+        // loop to print
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
 }
