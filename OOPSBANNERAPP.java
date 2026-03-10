1cefa1bcbeb10339502413f69c9fb243a2427218
rohitsaig07-crypto <rohitsaig07@gmail.com>
2026-03-10 14:11:20 +0530

    uc7over

--- OOPSBANNERAPP.java
+++ OOPSBANNERAPP.java
@@ -2,30 +2,32 @@ public class OOPSBANNERAPP {
 
     public static void main(String[] args) {
 
-        String[] lines = new String[7];
+        // Inline Array Initialization with String.join()
+        String[] lines = {
 
-        lines[0] = String.join("",
-                "   ***   ", "   ***   ", "  ******  ", "   ***** ");
+                String.join("",
+                        "   ***   ", "   ***   ", "  ******  ", "   ***** "),
 
-        lines[1] = String.join("",
-                "  ** **  ", "  ** **  ", "  **   ** ", "  **   **");
+                String.join("",
+                        "  ** **  ", "  ** **  ", "  **   ** ", "  **   **"),
 
-        lines[2] = String.join("",
-                " **   ** ", " **   ** ", "  **   ** ", "  **     ");
+                String.join("",
+                        " **   ** ", " **   ** ", "  **   ** ", "  **     "),
 
-        lines[3] = String.join("",
-                " **   ** ", " **   ** ", "  ******  ", "   ***** ");
+                String.join("",
+                        " **   ** ", " **   ** ", "  ******  ", "   ***** "),
 
-        lines[4] = String.join("",
-                " **   ** ", " **   ** ", "  **      ", "       **");
+                String.join("",
+                        " **   ** ", " **   ** ", "  **      ", "       **"),
 
-        lines[5] = String.join("",
-                "  ** **  ", "  ** **  ", "  **      ", "  **   **");
+                String.join("",
+                        "  ** **  ", "  ** **  ", "  **      ", "  **   **"),
 
-        lines[6] = String.join("",
-                "   ***   ", "   ***   ", "  **      ", "   ***** ");
+                String.join("",
+                        "   ***   ", "   ***   ", "  **      ", "   ***** ")
+        };
 
-        // loop to print
+        // Enhanced for loop
         for (String line : lines) {
             System.out.println(line);
         }
